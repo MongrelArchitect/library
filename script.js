815f58c38a5c4db006f79fb065309a37d5a52f7b
Sean Eric Thomas <set@seanericthomas.com>
2023-01-15 08:16:10 -0800

    Style popup and fix bug where canceling edit screws up next one

--- script.js
+++ script.js
@@ -39,18 +39,26 @@ function drawEditFinished(currentBook) {
   editContainer.innerHTML = '';
   const editFinished = document.createElement('button');
   editFinished.setAttribute('id', 'edit-finished');
+  const popup = document.querySelector('.popup');
 
   if (currentBook.finished) {
     editFinished.textContent = 'FINISHED';
     editFinished.dataset.finished = true;
+    editFinished.classList.add('button-finished');
+    popup.classList.add('finished');
   } else {
     editFinished.textContent = 'NOT FINISHED';
     editFinished.dataset.finished = false;
+    editFinished.classList.add('button-not-finished');
+    popup.classList.remove('finished');
   }
 
   editContainer.appendChild(editFinished);
 
   editFinished.addEventListener('click', () => {
+    editFinished.classList.toggle('button-finished');
+    editFinished.classList.toggle('button-not-finished');
+    popup.classList.toggle('finished');
     if (editFinished.dataset.finished === 'true') {
       editFinished.textContent = 'NOT FINISHED';
       editFinished.dataset.finished = false;
@@ -59,6 +67,11 @@ function drawEditFinished(currentBook) {
       editFinished.dataset.finished = true;
     }
   });
+}
+
+function handleCancel() {
+  const grayout = document.querySelector('.grayout');
+  grayout.setAttribute('style', 'display:none;');
 }
 
 function handleEdit(e) {
@@ -82,7 +95,14 @@ function handleEdit(e) {
   // Keep track of index for submiting edits
   submit.dataset.index = e.target.dataset.index;
   submit.addEventListener('click', submitEdit);
+
+  // Cancel editing
+  const cancel = document.querySelector('#edit-cancel');
+  cancel.addEventListener('click', handleCancel);
+
+  // XXX Relying on hoisting here...
   submit.addEventListener('click', drawLibrary);
+  cancel.addEventListener('click', drawLibrary);
 }
 
 function drawLibrary() {
@@ -96,11 +116,13 @@ function drawLibrary() {
     newBook.dataset.index = i;
 
     const newTitle = document.createElement('p');
+    newTitle.classList.add('book-title');
     newTitle.textContent = library[i].title;
     newBook.appendChild(newTitle);
 
     const newAuthor = document.createElement('p');
     newAuthor.textContent = `by ${library[i].author}`;
+    newAuthor.classList.add('book-author');
     newBook.appendChild(newAuthor);
 
     const newPages = document.createElement('p');
@@ -108,29 +130,32 @@ function drawLibrary() {
     const newFinished = document.createElement('span');
     if (library[i].finished) {
       newFinished.textContent = 'Finished';
+      newBook.classList.add('finished');
     } else {
       newFinished.textContent = 'Not finished';
+      newBook.classList.remove('finished');
     }
     newBook.appendChild(newPages);
     newBook.appendChild(newFinished);
 
     const newOptions = document.createElement('div');
+    newOptions.classList.add('options');
     const editButton = document.createElement('button');
+    editButton.classList.add('book-edit');
     const deleteButton = document.createElement('button');
     editButton.dataset.index = i;
     editButton.addEventListener('click', (e) => {
       handleEdit(e);
     });
     editButton.setAttribute('type', 'button');
-    editButton.textContent = 'EDIT';
     // Keep track for deleting book
     deleteButton.dataset.index = i;
+    deleteButton.classList.add('book-delete');
     deleteButton.addEventListener('click', (e) => {
       handleDelete(e);
       drawLibrary();
     });
     deleteButton.setAttribute('type', 'button');
-    deleteButton.textContent = 'DELETE';
 
     newOptions.appendChild(editButton);
     newOptions.appendChild(deleteButton);
@@ -138,22 +163,6 @@ function drawLibrary() {
 
     librarySection.appendChild(newBook);
   }
-}
-
-function handleCancel() {
-  const grayout = document.querySelector('.grayout');
-  const editSubmit = document.querySelector('#edit-submit');
-  grayout.setAttribute('style', 'display:none;');
-  editSubmit.removeEventListener('click', submitEdit);
-}
-
-// Cancel editing
-function cancelEdit() {
-  const cancel = document.querySelector('#edit-cancel');
-  cancel.addEventListener('click', () => {
-    handleCancel();
-    drawLibrary();
-  });
 }
 
 function createNewBook() {
@@ -183,17 +192,14 @@ function clearPopup() {
 }
 
 // Some default books to get the ball rollin'
-const bookOne = new Book('The Book of the New Sun', 'Gene Wolfe', 950, true);
+const bookOne = new Book('Book of the New Sun', 'Gene Wolfe', 950, true);
 const bookTwo = new Book('Human Action', 'Ludwig von Mises', 881, false);
-const bookThree = new Book('Anatomy of the State', 'Murray Rothbard', 62, true);
-const bookFour = new Book('Marooned in Realtime', 'Vernor Vinge', 270, true);
+const bookThree = new Book('Marooned in Realtime', 'Vernor Vinge', 270, true);
 
 bookOne.addToLibrary();
 bookTwo.addToLibrary();
 bookThree.addToLibrary();
-bookFour.addToLibrary();
 
 drawLibrary();
 clearPopup();
 createNewBook();
-cancelEdit();
